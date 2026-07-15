f249263386676d30355e61295cdbc8b059037c42
Prabhu-SNDPR <prabhuarumugam.cs@gmail.com>
2026-07-15 22:15:49 +0530

    Update warning message for missing logo image

--- SNDPR-t1.py
+++ SNDPR-t1.py
@@ -36,7 +36,7 @@ LOGO_FILENAME = "SNDPRlogo.png"  # <-- Change this to your logo's file name
 if os.path.exists(LOGO_FILENAME):
     st.image(LOGO_FILENAME, width=150) 
 else:
-    st.warning(f"Logo image '{LOGO_FILENAME}' not found in the repository folder.")
+   st.warning("Logo image 'SNDPR-logo.png' not found in the repository folder.")
 
 # ----------------------------------------
 
